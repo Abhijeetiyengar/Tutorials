f7f88843ddc286089c6b324fdfde6ea41aa41369
Abhijeetiyengar <Abhijeetiyengar@users.noreply.github.com>
2023-11-03 14:24:46 +0530

    Create SimpleThreadClass.java

---
+++ SimpleThreadClass.java
@@ -0,0 +1,9 @@
+public class SimpleThreadClass {
+    public static void main(String args[]) {
+      Thread th = new Thread (new Runnable() {
+          public void run() {
+              System.out.println("Hello");
+          }
+      });
+      th.start();
+    }
