c6a045f0f32f2fc70abd0517cd883a6a1051f086
abhi <abhijeet.iyengar@gmail.com>
2023-11-05 10:07:41 +0530

    New Version

--- SynchronizedTestThreadPool.java
+++ SynchronizedTestThreadPool.java
@@ -5,6 +5,7 @@ public class SynchronizedTestThreadPool {
     
     static class WorkerThread implements Runnable {  
         private String message;  
+        private static int counter = 0;
         public WorkerThread(String s){  
             this.message=s;  
         }  
@@ -13,11 +14,7 @@ public class SynchronizedTestThreadPool {
         } 
         public static synchronized void performActions(String message) {
             System.out.println(Thread.currentThread().getName()+" (Start) message = "+message);  
-            try {  
-                    Thread.sleep(2000); 
-                } catch (InterruptedException e) { 
-                    e.printStackTrace(); 
-                } 
+            System.out.println("Counter is : " + ++counter);
             System.out.println(Thread.currentThread().getName()+" (End) message = "+message); 
 
         }
--- TestThreadPool.java
+++ TestThreadPool.java
@@ -1,23 +1,20 @@
 import java.util.concurrent.ExecutorService;  
 import java.util.concurrent.Executors;  
 
-public class SynchronizedTestThreadPool {
+public class TestThreadPool {
     
     static class WorkerThread implements Runnable {  
         private String message;  
+        private static int counter = 0;
         public WorkerThread(String s){  
             this.message=s;  
         }  
          public void run() {  
              WorkerThread.performActions(this.message);
         } 
-        public synchronized void performActions(String message) {
+        public static void performActions(String message) {
             System.out.println(Thread.currentThread().getName()+" (Start) message = "+message);  
-            try {  
-                    Thread.sleep(2000); 
-                } catch (InterruptedException e) { 
-                    e.printStackTrace(); 
-                } 
+            System.out.println("Counter is : " + ++counter); 
             System.out.println(Thread.currentThread().getName()+" (End) message = "+message); 
 
         }
